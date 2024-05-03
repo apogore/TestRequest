5427783b279f9a4e07b725513811581eab183def
Aleksandr Pogorelov <apogore@sfedu.ru>
2024-05-03 15:30:34 +0300

    improve authorization

--- react-app/src/api/axios.js
+++ react-app/src/api/axios.js
@@ -1,17 +1,24 @@
-// api/axios.js
 import axios from 'axios';
 
 const axiosInstance = axios.create({
     baseURL: localStorage.getItem('api') || "https://artefomak-001-site1.ftempurl.com/api",
-})
+});
 
-axiosInstance.defaults.headers.common['Authorization'] = 'Basic ' + btoa('11174512:60-dayfreetrial');
+const authenticate = () => {
+    const username = '11174512';
+    const password = '60-dayfreetrial';
+    const token = btoa(`${username}:${password}`);
+    axiosInstance.defaults.headers.common['Authorization'] = `Basic ${token}`;
+};
+
+// Вызываем функцию аутентификации при импорте модуля
+authenticate();
 
 export const updateApiUrl = (newApiUrl) => {
     // Обновляем базовый URL Axios-инстанса
     axiosInstance.defaults.baseURL = newApiUrl;
     // Сохраняем новое значение базового URL в локальное хранилище
     localStorage.setItem('api', newApiUrl);
-}
+};
 
 export default axiosInstance;
